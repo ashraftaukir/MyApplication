cf133225c82052f0c6405e06745293d43649c06c
ashraftaukir <ashraftaukir@gmail.com>
2017-04-18 15:36:55 +0600

    testclass has been added

--- app/src/main/java/com/example/hello/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hello/myapplication/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.hello.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String TAG="TESTING";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        accessinggithub();
 
+    }
 
+    private void accessinggithub() {
+
+        Log.d(TAG, "accessinggithub: "+"hello github");
     }
 }
---
+++ app/src/main/java/com/example/hello/myapplication/TestClass.java
@@ -0,0 +1,8 @@
+package com.example.hello.myapplication;
+
+/**
+ * Created by hello on 4/18/17.
+ */
+
+public class TestClass {
+}
